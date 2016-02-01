73bb5b7f7e89bdd470a963a6b5ef794852054ad8
System Administrator <root@Rahuls-MacBook-Air.local>
2016-02-01 17:12:12 -0600

    added map view

--- www/js/controllers/EveryoneController.js
+++ www/js/controllers/EveryoneController.js
@@ -1,5 +1,22 @@
-
 angular.module('starter').controller('EveryoneController', function($scope) {
-	console.log("in EveryoneController");
+    console.log("in EveryoneController");
+    var ref = new Firebase("https://thevibe.firebaseio.com/");
+    var auth = ref.getAuth();
+    $scope.loggedIn = false;
+    $scope.createEMADisabled = false;
 
+    //init
+    var init = function() {
+        if (!auth) {
+            $scope.loginModal.show();
+        } else {
+            $scope.currentUser = auth;
+            $scope.loggedIn = true;
+            $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 15 };
+        }
+    };
+
+
+
+    init();
 });
